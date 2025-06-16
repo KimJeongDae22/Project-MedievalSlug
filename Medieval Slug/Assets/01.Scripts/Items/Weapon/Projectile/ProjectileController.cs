70ecb11cf7ab052726b846bff009bb9723af7e4c
박원희 <spilfive3@gmail.com>
2025-06-16 17:10:06 +0900

    [Fix] UI
    UI 최신화 했습니다.

--- Medieval Slug/Assets/01.Scripts/Items/Weapon/Projectile/ProjectileController.cs
+++ Medieval Slug/Assets/01.Scripts/Items/Weapon/Projectile/ProjectileController.cs
@@ -13,7 +13,6 @@ public class ProjectileController : MonoBehaviour, IPoolable
     private float curduration;
 
     private Action<GameObject> returnToPool;
-    [SerializeField] private EffectType curEffectType;
 
     void Awake()
     {
