b073626e1e637d5e90c910ee8d7fbaefd841163c
고윤아 <0205ria@gmail.com>
2025-06-16 11:35:18 +0900

    [Fix] 아이템 효과 적용 로직 연결
    * 무기 아이템 습득 시 플레이어 무기 변경
    * 체력 아이템 습득 시 플레이어 체력 변경
    * TODO: GameManager 점수 연결

--- Medieval Slug/Assets/01.Scripts/Managers/CharacterManager.cs
+++ Medieval Slug/Assets/01.Scripts/Managers/CharacterManager.cs
@@ -10,12 +10,21 @@ public class CharacterManager : Singleton<CharacterManager>
     public PlayerController Controller { get; private set; }
     public PlayerStatHandler StatHandler { get; private set; }
     public PlayerRangedHandler PlayerRangedHandler { get; private set; }
-
     public PlayerItemCollector PlayerItemCollector { get; private set; }
+    
     protected override void Awake()
     {
         base.Awake();
+        
         Controller = FindObjectOfType<PlayerController>();
+        StatHandler = FindObjectOfType<PlayerStatHandler>();
+        PlayerRangedHandler = FindObjectOfType<PlayerRangedHandler>();
+        PlayerItemCollector = FindObjectOfType<PlayerItemCollector>();
+        
+        if (Controller == null) Debug.LogError("CharacterManager: No Player Controller");
+        if (StatHandler == null) Debug.LogError("CharacterManager: No Stat Handler");
+        if (PlayerRangedHandler == null) Debug.LogError("CharacterManager: No Player Ranged Handler");
+        if (PlayerItemCollector == null) Debug.LogError("CharacterManager: No Player Item Collector");  
     }
 
     protected override void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
